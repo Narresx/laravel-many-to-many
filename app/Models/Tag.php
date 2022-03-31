55e0a1181c425005451200f576ecd4b8982367c2
Narresx <92748159+Narresx@users.noreply.github.com>
2022-03-31 20:39:11 +0200

    add relation in db

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    return $this->belongsToMany('App\Models\Post');
+    
 }
--- database/migrations/2022_03_31_181623_create_tags_table.php
+++ database/migrations/2022_03_31_181623_create_tags_table.php
@@ -16,7 +16,7 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('label');
-            $table->char('color')->default('#ffffff');
+            $table->char('color', 7)->default('#ffffff');
             $table->timestamps();
         });
     }
