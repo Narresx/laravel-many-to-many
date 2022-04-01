bd15958c2ed29495df3c40f5ac68bb864f3f9c34
Narresx <92748159+Narresx@users.noreply.github.com>
2022-04-01 16:29:29 +0200

    fix image in index & show

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -7,29 +7,34 @@
             @forelse ($posts as $post)
                 <div class="col-4">
                     <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="{{ $post->image }}" alt="{{ asset("storage/$post->image") }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $post->title }}</h5>
-                            <p class="card-text">{{ $post->content }}</p>
-                            <p class="card-text">{{ $post->created_at }}</p>
-                            <p class="badge badge-pill badge-{{ $post->category->color }}">
-                                {{ $post->category->label }}
-                            </p>
-                            @forelse ($post->tags as $tag)
-                                <p class="badge badge-primary">{{ $tag->label }}</p>
-                            @empty <span class="badge badge-pill badge-secondary">Nessuna Tag</span>
-                            @endforelse
-                            <div class="d-flex justify-content-between">
-                                <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-primary">Dettagli</a>
-                                <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-warning">Modifica</a>
-                                <form action="{{ route('admin.posts.destroy', $post->id) }}" method="post"
-                                    id="delete_form">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button type="submit" class="btn btn-danger">Elimina</button>
-                                </form>
+                        @if (substr($post->image, 0, 4) == 'http')
+                        <img class="card-img-top" src="{{ $post->image }}" @else <img class="card-img-top"
+                                src="{{ asset("storage/$post->image") }}" @endif
+                            alt="{{ asset("storage/$post->image") }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $post->title }}</h5>
+                                <p class="card-text">{{ $post->content }}</p>
+                                <p class="card-text">{{ $post->created_at }}</p>
+                                <p class="badge badge-pill badge-{{ $post->category->color }}">
+                                    {{ $post->category->label }}
+                                </p>
+                                @forelse ($post->tags as $tag)
+                                    <p class="badge badge-primary">{{ $tag->label }}</p>
+                                @empty <span class="badge badge-pill badge-secondary">Nessuna Tag</span>
+                                @endforelse
+                                <div class="d-flex justify-content-between">
+                                    <a href="{{ route('admin.posts.show', $post->id) }}"
+                                        class="btn btn-primary">Dettagli</a>
+                                    <a href="{{ route('admin.posts.edit', $post->id) }}"
+                                        class="btn btn-warning">Modifica</a>
+                                    <form action="{{ route('admin.posts.destroy', $post->id) }}" method="post"
+                                        id="delete_form">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-danger">Elimina</button>
+                                    </form>
+                                </div>
                             </div>
-                        </div>
                     </div>
                 </div>
             @empty
